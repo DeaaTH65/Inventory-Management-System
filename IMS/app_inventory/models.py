d616e3dc95736bb7b3ed7dea9e7b9a20fb210ae6
Deaath65 <deaath65@gmail.com>
2023-07-25 13:09:53 +0545

    Product Model

--- IMS/app_inventory/models.py
+++ IMS/app_inventory/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    count = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    

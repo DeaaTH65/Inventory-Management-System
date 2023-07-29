bd99dbb471ec116876ac56302d4fecc79aeb35f6
Deaath65 <deaath65@gmail.com>
2023-07-29 14:20:52 +0545

    getProduct api

--- IMS/api/urls.py
+++ IMS/api/urls.py
@@ -5,4 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('products/', views.getProducts),
+    path('products/<str:pk>/', views.getProduct),
 ]
--- IMS/api/views.py
+++ IMS/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from app_inventory.models import Product
 
 
 
@@ -7,7 +9,21 @@ from rest_framework.response import Response
 def getRoutes(request):
     routes = [
         'GET /api',
-        'GET /api/users',
-        'GET /api/rooms/:id',
+        'GET /api/products',
+        'GET /api/products/:id',
     ]
     return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    rooms = Product.objects.all()
+    serializer = ProductSerializer(rooms, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    room = Product.objects.get(id=pk)
+    serializer = ProductSerializer(room, many=False)
+    return Response(serializer.data)
